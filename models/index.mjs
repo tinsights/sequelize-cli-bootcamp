645feec5895316186f0c43094ae1413b8158089f
michelle-mok <michellemokhuihui@gmail.com>
2021-07-05 17:33:56 +0800

    initial commit

---
+++ models/index.mjs
@@ -0,0 +1,17 @@
+import { Sequelize } from 'sequelize';
+import allConfig from '../config/config.js';
+
+// import model files here
+
+const env = process.env.NODE_ENV || 'development';
+const config = allConfig[env];
+const db = {};
+
+let sequelize = new Sequelize(config.database, config.username, config.password, config);
+
+// add your model definitions to db here
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+export default db;
