b380b57d0cb457fa306e45adcc5808025c3c7857
michelle-mok <michellemokhuihui@gmail.com>
2021-07-05 18:04:05 +0800

    changed username and database name in config.js file

--- config/config.js
+++ config/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   development: {
-    username: 'michellemok',
+    username: '<YOUR_USERNAME>',
     password: null,
-    database: 'holiday_development',
+    database: '<YOUR_DATABASE_NAME>',
     host: '127.0.0.1',
     dialect: 'postgres',
   },
